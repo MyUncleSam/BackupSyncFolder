9590125d4611bedc1b9ddcdece70a1720b563879
Stefan Ruepp <s.ruepp@lobodms.com>
2018-02-16 21:30:26 +0100

    -

---
+++ BackupSyncFolder/AppArguments.cs
@@ -0,0 +1,19 @@
+﻿using CommandLineParser.Arguments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackupSyncFolder
+{
+	public class AppArguments
+	{
+		public static AppArguments CurArgs = new AppArguments();
+		
+		[DirectoryArgument('p', AllowMultiple = false, Description = "Path to your backup folder - make sure it is empty!", DirectoryMustExist = true, Optional = false)]
+		public System.IO.DirectoryInfo BackupPath;
+
+		[ValueArgument(typeof(int), 'd', AllowMultiple = false, DefaultValue = 2, Description = "Amount of duplicate backup directories.", Optional = true, ValueOptional = false)]
+		public int Duplicates;
+	}
+}
---
+++ BackupSyncFolder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackupSyncFolder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+			parser.AcceptEqualSignSyntaxForValueArguments = true;
+			parser.IgnoreCase = true;
+			parser.AcceptSlash = true;
+			parser.AllowShortSwitchGrouping = false;
+			parser.ExtractArgumentAttributes(AppArguments.CurArgs);
+
+			try
+			{
+				parser.ParseCommandLine(args);
+				parser.ShowParsedArguments();
+			}
+			catch (CommandLineParser.Exceptions.CommandLineArgumentException cle)
+			{
+				Console.WriteLine(string.Format("Invalid command line parameters: {0}", cle.Message));
+				parser.ShowUsage();
+				Environment.ExitCode = 1;
+				return;
+			}
+			catch (CommandLineParser.Validation.ArgumentConflictException ace)
+			{
+				Console.WriteLine(string.Format("Invalid command line parameters: {0}", ace.Message));
+				parser.ShowUsage();
+				Environment.ExitCode = 1;
+				return;
+			}
+
+			if (!parser.ParsingSucceeded)
+			{
+				Environment.ExitCode = 1;
+				return;
+			}  
+
+
+		}
+	}
+}
